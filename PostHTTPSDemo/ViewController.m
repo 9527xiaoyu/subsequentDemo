321c79081b315b06faffc24b6cbac4ba0e4d420a
9527xiaoyu <1446899815@qq.com>
2017-08-10 09:36:38 +0800

    Initial Commit

---
+++ PostHTTPSDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PostHTTPSDemo
+//
+//  Created by 杨晓宇 on 2017/8/10.
+//  Copyright © 2017年 com.ywart. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ PostHTTPSDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PostHTTPSDemo
+//
+//  Created by 杨晓宇 on 2017/8/10.
+//  Copyright © 2017年 com.ywart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
